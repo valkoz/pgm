5e892ad5d680d19336c6352865dd0d0f65f79c11
valentin <valentin.kozlov@nami.ru>
2019-04-19 15:57:21 +0300

    add enums

--- src/main/java/Loan.scala
+++ src/main/java/Loan.scala
@@ -1,64 +1,97 @@
 import com.cra.figaro.language.{Flip, Select, Universe}
 import com.cra.figaro.library.compound.CPD
 
+object Income extends Enumeration {
+  val High, Medium, Low = Value
+}
+
+object Age extends Enumeration {
+  val Young, MiddleAged, Old = Value
+}
+
+object RodToIncome extends Enumeration {
+  val High, Low = Value
+}
+
+object Assets extends Enumeration {
+  val High, Medium, Low = Value
+}
+
+object PaymentHistory extends Enumeration {
+  val Excellent, Acceptable, Unacceptable = Value
+}
+
+object FutureIncome extends Enumeration {
+  val Promising, NotPromising = Value
+}
+
+object Reliability extends Enumeration {
+  val Reliable, Unreliable = Value
+}
+
+object CreditWorthiness extends Enumeration {
+  val Positive, Negative = Value
+}
+
+
 object Loan {
   Universe.createNew()
 
-  private val income = Select(0.2 -> "High", 0.5 -> "Medium", 0.3 -> "Low")
+  private val income = Select(0.2 -> Income.High, 0.5 -> Income.Medium, 0.3 -> Income.Low)
 
-  private val age = Select(0.2 -> "18..25", 0.7 -> "25..60", 0.1 -> "60+")
+  private val age = Select(0.2 -> Age.Young, 0.7 -> Age.MiddleAged, 0.1 -> Age.Old)
 
-  private val rodToIncome = Select(0.3 -> "LowR", 0.7 -> "HighR")
+  private val rodToIncome = Select(0.3 -> RodToIncome.Low, 0.7 -> RodToIncome.High)
 
   private val assets = CPD(income,
-    "High" -> Select(0.6 -> "HighA", 0.3 -> "MediumA", 0.1 -> "LowA"),
-    "Medium" -> Select(0.3 -> "HighA", 0.4 -> "MediumA", 0.3 -> "LowA"),
-    "Low" -> Select(0.1 -> "HighA", 0.4 -> "MediumA", 0.5 -> "LowA")
+    Income.High -> Select(0.6 -> Assets.High, 0.3 -> Assets.Medium, 0.1 -> Assets.Low),
+    Income.Medium  -> Select(0.3 -> Assets.High, 0.4 -> Assets.Medium, 0.3 -> Assets.Low),
+    Income.Low  -> Select(0.1 -> Assets.High, 0.4 -> Assets.Medium, 0.5 -> Assets.Low)
   )
 
   private val paymentHistory = CPD(age, rodToIncome,
-    ("18..25", "LowR") -> Select(0.6 -> "Excellent", 0.3 -> "Acceptable", 0.1 -> "Unacceptable"),
-    ("18..25", "HighR") -> Select(0.6 -> "Excellent", 0.3 -> "Acceptable", 0.1 -> "Unacceptable"),
-    ("25..60", "LowR") -> Select(0.6 -> "Excellent", 0.3 -> "Acceptable", 0.1 -> "Unacceptable"),
-    ("25..60", "HighR") -> Select(0.6 -> "Excellent", 0.3 -> "Acceptable", 0.1 -> "Unacceptable"),
-    ("60+", "LowR") -> Select(0.6 -> "Excellent", 0.3 -> "Acceptable", 0.1 -> "Unacceptable"),
-    ("60+", "HighR") -> Select(0.6 -> "Excellent", 0.3 -> "Acceptable", 0.1 -> "Unacceptable")
+    (Age.Young, RodToIncome.Low) -> Select(0.6 -> PaymentHistory.Excellent, 0.3 -> PaymentHistory.Acceptable, 0.1 -> PaymentHistory.Unacceptable),
+    (Age.Young, RodToIncome.High) -> Select(0.6 -> PaymentHistory.Excellent, 0.3 -> PaymentHistory.Acceptable, 0.1 -> PaymentHistory.Unacceptable),
+    (Age.MiddleAged, RodToIncome.Low) -> Select(0.6 -> PaymentHistory.Excellent, 0.3 -> PaymentHistory.Acceptable, 0.1 -> PaymentHistory.Unacceptable),
+    (Age.MiddleAged, RodToIncome.High) -> Select(0.6 -> PaymentHistory.Excellent, 0.3 -> PaymentHistory.Acceptable, 0.1 -> PaymentHistory.Unacceptable),
+    (Age.Old, RodToIncome.Low) -> Select(0.6 -> PaymentHistory.Excellent, 0.3 -> PaymentHistory.Acceptable, 0.1 -> PaymentHistory.Unacceptable),
+    (Age.Old, RodToIncome.High) -> Select(0.6 -> PaymentHistory.Excellent, 0.3 -> PaymentHistory.Acceptable, 0.1 -> PaymentHistory.Unacceptable)
   )
 
 
   private val futureIncome = CPD(assets, income,
-    ("HighA", "High") -> Select(0.6 -> "Promising", 0.4 -> "Not_promising"),
-    ("HighA", "Medium") -> Select(0.6 -> "Promising", 0.4 -> "Not_promising"),
-    ("HighA", "Low") -> Select(0.6 -> "Promising", 0.4 -> "Not_promising"),
-    ("MediumA", "High") -> Select(0.6 -> "Promising", 0.4 -> "Not_promising"),
-    ("MediumA", "Medium") -> Select(0.6 -> "Promising", 0.4 -> "Not_promising"),
-    ("MediumA", "Low") -> Select(0.6 -> "Promising", 0.4 -> "Not_promising"),
-    ("LowA", "High") -> Select(0.6 -> "Promising", 0.4 -> "Not_promising"),
-    ("LowA", "Medium") -> Select(0.6 -> "Promising", 0.4 -> "Not_promising"),
-    ("LowA", "Low") -> Select(0.6 -> "Promising", 0.4 -> "Not_promising")
+    (Assets.High, Income.High) -> Select(0.6 -> FutureIncome.Promising, 0.4 -> FutureIncome.NotPromising),
+    (Assets.High, Income.Medium) -> Select(0.6 -> FutureIncome.Promising, 0.4 -> FutureIncome.NotPromising),
+    (Assets.High, Income.Low) -> Select(0.6 -> FutureIncome.Promising, 0.4 -> FutureIncome.NotPromising),
+    (Assets.Medium, Income.High) -> Select(0.6 -> FutureIncome.Promising, 0.4 -> FutureIncome.NotPromising),
+    (Assets.Medium, Income.Medium) -> Select(0.6 -> FutureIncome.Promising, 0.4 -> FutureIncome.NotPromising),
+    (Assets.Medium, Income.Low) -> Select(0.6 -> FutureIncome.Promising, 0.4 -> FutureIncome.NotPromising),
+    (Assets.Low, Income.High) -> Select(0.6 -> FutureIncome.Promising, 0.4 -> FutureIncome.NotPromising),
+    (Assets.Low, Income.Medium) -> Select(0.6 -> FutureIncome.Promising, 0.4 -> FutureIncome.NotPromising),
+    (Assets.Low, Income.Low) -> Select(0.6 -> FutureIncome.Promising, 0.4 -> FutureIncome.NotPromising)
   )
 
   private val reliability = CPD(paymentHistory, age,
-    ("Excellent", "18..25") -> Select(0.6 -> "Reliable", 0.4 -> "Unreliable"),
-    ("Excellent", "25..60") -> Select(0.6 -> "Reliable", 0.4 -> "Unreliable"),
-    ("Excellent", "60+") -> Select(0.6 -> "Reliable", 0.4 -> "Unreliable"),
-    ("Acceptable", "18..25") -> Select(0.6 -> "Reliable", 0.4 -> "Unreliable"),
-    ("Acceptable", "25..60") -> Select(0.6 -> "Reliable", 0.4 -> "Unreliable"),
-    ("Acceptable", "60+") -> Select(0.6 -> "Reliable", 0.4 -> "Unreliable"),
-    ("Unacceptable", "18..25") -> Select(0.6 -> "Reliable", 0.4 -> "Unreliable"),
-    ("Unacceptable", "25..60") -> Select(0.6 -> "Reliable", 0.4 -> "Unreliable"),
-    ("Unacceptable", "60+") -> Select(0.6 -> "Reliable", 0.4 -> "Unreliable")
+    (PaymentHistory.Excellent, Age.Young) -> Select(0.6 -> Reliability.Reliable, 0.4 -> Reliability.Unreliable),
+    (PaymentHistory.Excellent, Age.MiddleAged) -> Select(0.6 -> Reliability.Reliable, 0.4 -> Reliability.Unreliable),
+    (PaymentHistory.Excellent, Age.Old) -> Select(0.6 -> Reliability.Reliable, 0.4 -> Reliability.Unreliable),
+    (PaymentHistory.Acceptable, Age.Young) -> Select(0.6 -> Reliability.Reliable, 0.4 -> Reliability.Unreliable),
+    (PaymentHistory.Acceptable, Age.MiddleAged) -> Select(0.6 -> Reliability.Reliable, 0.4 -> Reliability.Unreliable),
+    (PaymentHistory.Acceptable, Age.Old) -> Select(0.6 -> Reliability.Reliable, 0.4 -> Reliability.Unreliable),
+    (PaymentHistory.Unacceptable, Age.Young) -> Select(0.6 -> Reliability.Reliable, 0.4 -> Reliability.Unreliable),
+    (PaymentHistory.Unacceptable, Age.MiddleAged) -> Select(0.6 -> Reliability.Reliable, 0.4 -> Reliability.Unreliable),
+    (PaymentHistory.Unacceptable, Age.Old) -> Select(0.6 -> Reliability.Reliable, 0.4 -> Reliability.Unreliable)
   )
 
   private val creditWorthiness = CPD(reliability, futureIncome, rodToIncome,
-    ("Reliable", "Promising","LowR") -> Select(0.6 -> "Positive", 0.4 -> "Negative"),
-    ("Reliable", "Promising","HighR") -> Select(0.6 -> "Positive", 0.4 -> "Negative"),
-    ("Reliable", "Not_promising","LowR") -> Select(0.6 -> "Positive", 0.4 -> "Negative"),
-    ("Reliable", "Not_promising","HighR") -> Select(0.6 -> "Positive", 0.4 -> "Negative"),
-    ("Unreliable", "Promising","LowR") -> Select(0.6 -> "Positive", 0.4 -> "Negative"),
-    ("Unreliable", "Promising","HighR") -> Select(0.6 -> "Positive", 0.4 -> "Negative"),
-    ("Unreliable", "Not_promising","LowR") -> Select(0.6 -> "Positive", 0.4 -> "Negative"),
-    ("Unreliable", "Not_promising","HighR") -> Select(0.6 -> "Positive", 0.4 -> "Negative"),
+    (Reliability.Reliable, FutureIncome.Promising,RodToIncome.Low) -> Select(0.6 -> CreditWorthiness.Positive, 0.4 -> CreditWorthiness.Negative),
+    (Reliability.Reliable, FutureIncome.Promising, RodToIncome.High) -> Select(0.6 -> CreditWorthiness.Positive, 0.4 -> CreditWorthiness.Negative),
+    (Reliability.Reliable, FutureIncome.NotPromising,RodToIncome.Low) -> Select(0.6 -> CreditWorthiness.Positive, 0.4 -> CreditWorthiness.Negative),
+    (Reliability.Reliable, FutureIncome.NotPromising, RodToIncome.High) -> Select(0.6 -> CreditWorthiness.Positive, 0.4 -> CreditWorthiness.Negative),
+    (Reliability.Unreliable, FutureIncome.Promising,RodToIncome.Low) -> Select(0.6 -> CreditWorthiness.Positive, 0.4 -> CreditWorthiness.Negative),
+    (Reliability.Unreliable, FutureIncome.Promising, RodToIncome.High) -> Select(0.6 -> CreditWorthiness.Positive, 0.4 -> CreditWorthiness.Negative),
+    (Reliability.Unreliable, FutureIncome.NotPromising,RodToIncome.Low) -> Select(0.6 -> CreditWorthiness.Positive, 0.4 -> CreditWorthiness.Negative),
+    (Reliability.Unreliable, FutureIncome.NotPromising, RodToIncome.High) -> Select(0.6 -> CreditWorthiness.Positive, 0.4 -> CreditWorthiness.Negative),
 
   )
 
